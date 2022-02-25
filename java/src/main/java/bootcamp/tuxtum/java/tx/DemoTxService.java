7f31c20f45a7e275511f14c9eb6f7e6bd043a593
gambitmis <gambitmis@gmail.com>
2022-02-26 00:26:30 +0700

    Issue#2 Transaction Pitfall - Big Transaction

--- java/src/main/java/bootcamp/tuxtum/java/tx/DemoTxService.java
+++ java/src/main/java/bootcamp/tuxtum/java/tx/DemoTxService.java
@@ -12,11 +12,18 @@ public class DemoTxService {
     @Autowired
     private UserRepository userRepository;
 
-    @Transactional
     public void callRepository() {
-        //User user1 = new User();
-        //user1.setId(1);
-        //user1.setName("user 1");
+        step1();
+        step2();
+        processLongRunning();
+    }
+
+    public void processLongRunning(){
+    }
+
+    @Transactional
+    public void step1(){
+
         User user1 = new User(1,"user 1");
         User user2 = new User(2,"user 2");
         User user3 = new User(3,"user 3");
@@ -24,6 +31,15 @@ public class DemoTxService {
         userRepository.save(user1);
         userRepository.save(user2);
         userRepository.save(user3);
+    }
 
+    @Transactional
+    public void step2(){
+
+        User user3 = new User(3,"user 3");
+
+        userRepository.save(user3);
+        userRepository.save(user3);
+        userRepository.save(user3);
     }
 }
