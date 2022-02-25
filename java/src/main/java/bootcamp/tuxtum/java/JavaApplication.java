da636af84c753fd4438ad7bee37481731b0f7927
gambitmis <gambitmis@gmail.com>
2022-02-25 18:34:19 +0700

    Issue#2 Working with Repository

--- java/src/main/java/bootcamp/tuxtum/java/JavaApplication.java
+++ java/src/main/java/bootcamp/tuxtum/java/JavaApplication.java
@@ -1,10 +1,23 @@
 package bootcamp.tuxtum.java;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class JavaApplication {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostConstruct
+	public void xyz(){
+		User user = new User();
+		user.setName("chalermsak");
+		userRepository.save(user);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaApplication.class, args);
--- java/src/main/java/bootcamp/tuxtum/java/UserService.java
+++ java/src/main/java/bootcamp/tuxtum/java/UserService.java
@@ -1,10 +1,18 @@
 package bootcamp.tuxtum.java;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
     public String concatData(String name){
-        return "Hello "+ name;
+        Optional<User> result = userRepository.findByName(name);
+        return "Hello "+ result.get().getName();
     }
 }
