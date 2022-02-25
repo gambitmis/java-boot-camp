6dddf556a658547487112ef921c4e331ce02a4a2
gambitmis <gambitmis@gmail.com>
2022-02-25 11:30:13 +0700

    Issue#2 Create first RESTful API

---
+++ java/src/main/java/bootcamp/tuxtum/java/HelloController.java
@@ -0,0 +1,14 @@
+package bootcamp.tuxtum.java;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public HelloResponse sayHi(@PathVariable String name) {
+        return new HelloResponse("Hello "+ name);
+    }
+}
---
+++ java/src/main/java/bootcamp/tuxtum/java/HelloResponse.java
@@ -0,0 +1,17 @@
+package bootcamp.tuxtum.java;
+
+public class HelloResponse {
+    private String message;
+
+    public HelloResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
